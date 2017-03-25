329cb5cdd19aa0a825c0ffd87894f90f2d48ae01
Artyom <solomyanniy63@gmail.com>
2017-03-25 16:53:00 +0400

    A lot of changes...

---
+++ Remember/CardButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Remember
+{
+    public class CardButton:Button
+    {
+        private bool _shown = false;
+        private object _internalContent;
+
+
+        public bool Shown
+        {
+            get { return _shown; }
+            set
+            {
+                _shown = value;
+                if (_shown)
+                {
+                    Content = _internalContent;
+                }
+                else
+                {
+                    Content = null;
+                }
+            }
+        }
+
+        public void ReversShownLabel()
+        {
+            Shown = !Shown;
+        }
+
+//        public new object Content
+//        {
+//            get;  set; }
+
+        public object InternalContent
+        {
+            get { return _internalContent; }
+            set { _internalContent = value; }
+        }
+    }
+}
--- Remember/GameWindow.xaml.cs
+++ Remember/GameWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,35 +21,81 @@ namespace Remember
     /// </summary>
     public partial class GameWindow : Window
     {
-        private Button[,] _img;
-        private Button[] _tmpImages = new Button[2];
+        private CardButton[,] _img;
+        private CardButton[] _tmpImages = new CardButton[2];
 
+        private String[] _imageFiles;
+       
         public GameWindow(int width, int height)
         {
             InitializeComponent(width, height);
+            GetImages("C:\\Users\\solom\\Documents\\visual studio 2017\\Projects\\Remember\\src\\pictureSet1");
+        }
+
+        private void GetImages(String path)
+        {
+            _imageFiles = Directory.GetFiles(path);
+        }
+
+        private void imgBtn_Click(object sender, RoutedEventArgs e)
+        {
+            CardButton btn = (CardButton) sender;
+            int i = 0;
+            while (i < _tmpImages.Length && _tmpImages[i] != null)
+            {
+                i++;
+            }
+            switch (i)
+            {
+                case 2:
+                    for (int j = 0; j < 2; j++)
+                    {
+                        _tmpImages[j].Shown = false;
+                        _tmpImages[j] = null;
+                    }
+                    _tmpImages[0] = btn;
+                    btn.Shown = true;
+                    break;
+                case 0:
+                case 1:
+                    _tmpImages[i] = btn;
+                    btn.Shown = true;
+                    break;
+            }
+            if (_tmpImages[0] != null && _tmpImages[1] != null && _tmpImages[0].Content.ToString().Equals(_tmpImages[1].Content.ToString()))
+            {
+
+                for (int j = 0; j < 2; j++)
+                {
+                    _tmpImages[j].IsEnabled = false;
+                    _tmpImages[j].Content = null;
+                }
+            }
+
         }
 
         private void InitializeComponent(int width, int height)
         {
             InitializeComponent();
             UniformGrid.Columns = width;
-            _img = new Button[width, height];
+            _img = new CardButton[width,height];
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    _img[i, j] = new Button()
+                    _img[i, j] = new CardButton()
                     {
                         HorizontalAlignment = HorizontalAlignment.Stretch,
                         VerticalAlignment = VerticalAlignment.Stretch,
                         
-                        Content = new Image()
+                        
+                        InternalContent = new Image()
                         {
                             Source = new BitmapImage(new Uri("C:\\Users\\solom\\Documents\\visual studio 2017\\Projects\\Remember\\src\\pictureSet1\\omNq46VJ8GQ.jpg"))
                         },
                         
                     };
-                    
+                    _img[i,j].Click += new RoutedEventHandler(this.imgBtn_Click);
                     UniformGrid.Children.Add(_img[i, j]);
                     
 
